5920b7098ca88e566d0526743aca1e7e9dcf3af0
Christian Panse <cp@fgcz.ethz.ch>
2024-07-01 14:00:14 +0200

    test: add install to test

--- tests/testthat.R
+++ tests/testthat.R
@@ -3,5 +3,12 @@
 library(testthat)                                                                                                                                             
                                                                                                                                                               
 suppressPackageStartupMessages(library(rawDiag))                                                                                                                
+
+rawDiag::checkRawrr()
+
+if (isFALSE(rawrr::.checkDllInMonoPath())){
+  rawrr::installRawFileReaderDLLs()
+}
+rawrr::installRawrrExe()
                                                                                                                                                               
 test_check("rawDiag")   
